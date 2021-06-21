cc4e541d6627621c33161a55e85442ea1aea9bf0
Sebastian Weidlinger <s.weidlinger99@gmx.at>
2021-06-21 10:52:32 +0200

    Initial Commit

---
+++ ProjectWeidlinger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProjectWeidlinger
+//
+//  Created by Sebastian Weidlinger on 21.06.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
